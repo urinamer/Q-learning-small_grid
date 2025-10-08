7a9a1e729477ce17344601afd4388d2301caaee2
arielhirsch <hirschariel190@gmail.com>
2025-10-08 22:40:12 +0300

    First commit

---
+++ src/Constants.java
@@ -0,0 +1,11 @@
+public final class Constants {
+
+
+    private Constants(){
+        //No Instance
+    }
+
+    public static final double LEARNING_RATE = 0.5;
+    public static final double DISCOUNT_FACTOR = 0.9;
+
+}
